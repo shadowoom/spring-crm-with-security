3f997b41435749bd11722ccb999b9ed76264cbb6
Zhang Chen <zhangchen.happy@gmail.com>
2018-02-08 12:07:05 +0800

    Simple Spring CRM Project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Spring CRM Project
+# Spring + Spring MVC + Hibernate
+# Spring Security Handles Authentication and Authorization
+# AOP handles Logging
+# Managed by Maven
+
---
+++ src/main/java/com/luv2code/springdemo/config/DemoSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.luv2code.springdemo.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// add a reference to our security data source 
+	
+	@Autowired
+	private DataSource securityDataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// add our users for in memory authentication
+		
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//		
+//		auth.inMemoryAuthentication()
+//			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+//			.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
+//			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+			.antMatchers("/customer/showForm*").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers("/customer/save*").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers("/customer/delete").hasRole("ADMIN")
+			.antMatchers("/customer/**").hasRole("EMPLOYEE")
+			.antMatchers("/resources/**").permitAll()
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll()
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
+		
+	}
+		
+}
+
+
+
+
+
+
